1a57920458a50c9d170c72eb5beb770f4bc425d8
Michael Fernandez <mikevfern@gmail.com>
2021-03-01 21:33:47 -0800

    add header elements, and init header style

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import '../styles/Header.css'
 import AppsIcon from '@material-ui/icons/Apps';
 import Avatar from '@material-ui/core/Avatar';
 import MenuIcon from '@material-ui/icons/Menu';
@@ -9,14 +10,20 @@ import VideoCallIcon from '@material-ui/icons/VideoCall';
 const Header = () => {
     return (
         <div className="header">
-            <MenuIcon />
-            <img className="header__logo" alt="logo" src="https://upload.wikimedia.org/wikipedia/commons/b/b8/YouTube_Logo_2017.svg"/>
-            <input type="text"></input>
-            <SearchIcon />
-            <VideoCallIcon />
-            <AppsIcon />
-            <NotificationsIcon />
-            <Avatar alt="avatar" src="https://media-exp1.licdn.com/dms/image/C4D03AQEIBrAAXnklmg/profile-displayphoto-shrink_800_800/0/1517611169022?e=1620259200&v=beta&t=B5DsTWLN8OTbYOYpPJ0xGzPqHTnkZSctQCjZuneIn5w"/>
+            <div className="header__left">
+                <MenuIcon />
+                <img className="header__logo" alt="logo" src="https://upload.wikimedia.org/wikipedia/commons/b/b8/YouTube_Logo_2017.svg"/>
+            </div>
+            <div className="header__middle">
+                <input type="text"></input>
+                <SearchIcon />
+            </div>
+            <div className="header__right">
+                <VideoCallIcon />
+                <AppsIcon />
+                <NotificationsIcon />
+                <Avatar alt="avatar" src="https://media-exp1.licdn.com/dms/image/C4D03AQEIBrAAXnklmg/profile-displayphoto-shrink_800_800/0/1517611169022?e=1620259200&v=beta&t=B5DsTWLN8OTbYOYpPJ0xGzPqHTnkZSctQCjZuneIn5w"/>
+            </div>
         </div>
     )
 }
