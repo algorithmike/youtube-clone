ce85b5a830f9b32cb32b48692bce88ce196f494e
Michael Fernandez <mikevfern@gmail.com>
2021-03-05 18:40:10 -0800

    adding sidebarRows

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,6 +2,12 @@ import React from 'react';
 import HomeIcon from '@material-ui/icons/Home';
 import WhatshotIcon from '@material-ui/icons/Whatshot';
 import SubscriptionsIcon from '@material-ui/icons/Subscriptions';
+import VideoLibraryIcon from '@material-ui/icons/VideoLibrary';
+import HistoryIcon from '@material-ui/icons/History';
+import OndemandVideoIcon from '@material-ui/icons/OndemandVideo';
+import WatchLaterIcon from '@material-ui/icons/WatchLater';
+import ThumbUpAltOutlinedIcon from '@material-ui/icons/ThumbUpAltOutlined';
+import ExpandMoreOutlinedIcon from '@material-ui/icons/ExpandMoreOutlined';
 
 import SidebarRow from './SidebarRow';
 import '../styles/Sidebar.css';
@@ -9,9 +15,17 @@ import '../styles/Sidebar.css';
 const Sidebar = () => {
     return (
         <div className="sidebar">
-            <SidebarRow Icon={HomeIcon} title="Home"/>
+            <SidebarRow Icon={HomeIcon} title="Home" selected/>
             <SidebarRow Icon={WhatshotIcon} title="Trending"/>
             <SidebarRow Icon={SubscriptionsIcon} title="Subscription"/>
+            <hr />
+            <SidebarRow Icon={VideoLibraryIcon} title="Library"/>
+            <SidebarRow Icon={HistoryIcon} title="History"/>
+            <SidebarRow Icon={OndemandVideoIcon} title="Your videos"/>
+            <SidebarRow Icon={WatchLaterIcon} title="Watch later"/>
+            <SidebarRow Icon={ThumbUpAltOutlinedIcon} title="Liked videos"/>
+            <SidebarRow Icon={ExpandMoreOutlinedIcon} title="Show more"/>
+            <hr />
         </div>
     )
 }
--- src/components/SidebarRow.js
+++ src/components/SidebarRow.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import '../styles/SidebarRow.css';
 
-const SidebarRow = ({Icon, title}) => {
+const SidebarRow = ({Icon, selected, title}) => {
     return (
-        <div className="sidebarRow">
+        <div className={`sidebarRow ${selected && 'selected'}`}>
             <Icon className="sidebarRow__icon"/>
             <h2 className="sidebarRow__title">{title}</h2>
         </div>
