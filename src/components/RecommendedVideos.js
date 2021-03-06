0daaff96d33ac495bc4bfef30ba0a8f4e8f15cff
Michael Fernandez <mikevfern@gmail.com>
2021-03-05 21:02:45 -0800

    recommendedVideos and videoCard styling

--- src/components/RecommendedVideos.js
+++ src/components/RecommendedVideos.js
@@ -8,12 +8,12 @@ const RecommendedVideos = () => {
     for(let x = 1; x <= 24; x++){
         dummyContent.push (
             <VideoCard 
-                image = "https://dummyimage.com/300x200/000/fff"
+                image = "https://dummyimage.com/600x400/000/fff"
                 channel = "Placeholder Channel"
                 channelImage = "https://dummyimage.com/200x200/ff47ff/ffffff&text=Avatar"
                 title = {`Video Title #${x}`}
                 views = "123,456,789"
-                timestamp = "Sometime Ago"
+                timestamp = "Time ago"
             />
         )
     }
