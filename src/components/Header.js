619efb86ffb30984d61847b462a71194bcfbfd73
Michael Fernandez <mikevfern@gmail.com>
2021-04-10 09:35:16 -0700

    remove input focus

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,46 +1,53 @@
-import React, { useState }from 'react'
-import '../styles/Header.css'
-import AppsIcon from '@material-ui/icons/Apps';
-import Avatar from '@material-ui/core/Avatar';
-import MenuIcon from '@material-ui/icons/Menu';
-import NotificationsIcon from '@material-ui/icons/Notifications';
-import SearchIcon from '@material-ui/icons/Search';
-import VideoCallIcon from '@material-ui/icons/VideoCall';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import "../styles/Header.css";
+import AppsIcon from "@material-ui/icons/Apps";
+import Avatar from "@material-ui/core/Avatar";
+import MenuIcon from "@material-ui/icons/Menu";
+import NotificationsIcon from "@material-ui/icons/Notifications";
+import SearchIcon from "@material-ui/icons/Search";
+import VideoCallIcon from "@material-ui/icons/VideoCall";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-    const [searchTerm, setSearchTerm] = useState('');
+  const [searchTerm, setSearchTerm] = useState("");
 
-    return (
-        <div className="header">
-            <div className="header__left">
-                <MenuIcon />
-                <Link to="/">
-                    <img className="header__logo" alt="logo" src="https://upload.wikimedia.org/wikipedia/commons/b/b8/YouTube_Logo_2017.svg"/>
-                </Link>
-            </div>
-            <div className="header__middle">
-                <input
-                    className="header__inputField"
-                    value={searchTerm}
-                    placeholder="Search"
-                    type="text"
-                    onChange={(event) => {setSearchTerm(event.target.value)}}
-                />
-                <Link to={`/search/${searchTerm}`}>
-                    <SearchIcon className="header__inputButton"/>
-                </Link>
-            </div>
-            <div className="header__right">
-                <VideoCallIcon className="header__icon"/>
-                <AppsIcon className="header__icon"/>
-                <NotificationsIcon className="header__icon"/>
-                <Avatar className="header__icon"
-                    alt="avatar"
-                    src="https://media-exp1.licdn.com/dms/image/C4D03AQEIBrAAXnklmg/profile-displayphoto-shrink_800_800/0/1517611169022?e=1620259200&v=beta&t=B5DsTWLN8OTbYOYpPJ0xGzPqHTnkZSctQCjZuneIn5w"/>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="header">
+      <div className="header__left">
+        <MenuIcon />
+        <Link to="/">
+          <img
+            className="header__logo"
+            alt="logo"
+            src="https://upload.wikimedia.org/wikipedia/commons/b/b8/YouTube_Logo_2017.svg"
+          />
+        </Link>
+      </div>
+      <div className="header__middle">
+        <input
+          value={searchTerm}
+          placeholder="Search"
+          type="text"
+          onChange={(event) => {
+            setSearchTerm(event.target.value);
+          }}
+        />
+        <Link to={`/search/${searchTerm}`}>
+          <SearchIcon className="header__inputButton" />
+        </Link>
+      </div>
+      <div className="header__right">
+        <VideoCallIcon className="header__icon" />
+        <AppsIcon className="header__icon" />
+        <NotificationsIcon className="header__icon" />
+        <Avatar
+          className="header__icon"
+          alt="avatar"
+          src="https://media-exp1.licdn.com/dms/image/C4D03AQEIBrAAXnklmg/profile-displayphoto-shrink_800_800/0/1517611169022?e=1620259200&v=beta&t=B5DsTWLN8OTbYOYpPJ0xGzPqHTnkZSctQCjZuneIn5w"
+        />
+      </div>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
